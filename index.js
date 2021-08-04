d5208143245a17f2fa77a609933e2d2ccb4c39c1
Lidi <lidiana.tdsc@gmail.com>
2021-08-04 18:39:58 -0300

    aula 28/7

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const { Tarefa } = require('./models');
+const routerTarefa = require('./routes/tarefas');
 
 app.use(bodyParser.json());
 
@@ -10,81 +10,6 @@ app.get('/', (req, res) => {
 });
 
 ////////////////////////Rotas das tarefas
-
-app.post('/tarefas', async (req, res) => {
-
-    try {
-        const tarefa = await Tarefa.create(req.body);
-        res.status(201).json(tarefa);        
-    } catch (error) {
-        res.status(500).json({message: 'Erro interno no servidor'}); 
-    }
-
-});
-
-app.get('/tarefas', async (req, res) => {
-
-    try {
-        const tarefas = await Tarefa.findAll();
-        res.status(200).json(tarefas);
-    } catch (error) {
-        res.status(500).json({message: 'Erro interno no servidor'});
-    }
-  
-});
-
-app.get('/tarefas/:id', async (req, res) => {
-
-    try {
-        const tarefa = await Tarefa.findAll({
-        where: {
-            id: req.params.id
-        }
-        });
-        if (tarefa.length == 0) {
-            res.status(404).json({ message: 'Tarefa não encontrada' });  
-        }
-        res.status(200).json(...tarefa);
-    } catch (error) {
-        res.status(500).json({message: 'Erro interno no servidor'});
-    }
-    
-});
-
-app.delete('/tarefas/:id', async (req, res) => {
-
-    try {
-        const result = await Tarefa.destroy({
-            where: {
-                id: req.params.id   
-            }
-        });
-        if (result == 0) {
-            res.status(404).json({ message: 'Tarefa não encontrada' });  
-        }
-        res.status(200).json({message:'Tarefa removida com sucesso'});   
-    } catch (error) {
-        res.status(500).json({message: 'Erro interno no servidor'});
-    }
-    
-});
-
-app.put('/tarefas/:id', async (req, res) => {
-
-    try {
-        const result = await Tarefa.update(req.body,{
-            where: {
-                id: req.params.id   
-            }
-        });
-        if (result[0] == 0) {
-            res.status(404).json({ message: 'Tarefa não encontrada' });  
-        }
-        res.status(200).json('Tarefa atualizada com sucesso!');        
-    } catch (error) {
-        res.status(500).json({message: 'Erro interno no servidor'});
-    }
-
-});
+app.use('/tarefas', routerTarefa)
 
 app.listen(4000);
